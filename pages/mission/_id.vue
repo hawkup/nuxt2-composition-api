f590f882d40f4e0fa187c4e57c954429b5904235
hawkup <spiritsquare@gmail.com>
2021-07-03 12:38:23 +0700

    Add nuxt-class-decorator and add mission, ship page

---
+++ pages/mission/_id.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    This is mission detail page
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class MissionDetail extends Vue {}
+</script>
---
+++ pages/mission/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    This is mission page
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class MissionIndex extends Vue {}
+</script>
