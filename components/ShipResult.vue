209541f65fe5bece74179e1785d73260786416ab
hawkup <spiritsquare@gmail.com>
2021-07-18 22:10:21 +0700

    feat: use swrv with axios (#8)

---
+++ components/ShipResult.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from '@nuxtjs/composition-api'
+import { AxiosRequestConfig } from 'axios'
+
+import useRequest from '~/composables/useRequest'
+
+export default defineComponent({
+  setup() {
+    const request = computed(() => {
+      const requestConfig: AxiosRequestConfig = {
+        method: 'get',
+        url: 'https://api.spacex.land/rest/ships-result'
+      }
+      return requestConfig
+    })
+
+    const { data, error } = useRequest(request)
+
+    return { data , error }
+  }
+})
+</script>
