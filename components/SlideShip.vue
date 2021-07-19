5c220d4cc9481c7a1d47c4d46145d0ddd8f21456
hawkup <spiritsquare@gmail.com>
2021-07-19 21:56:19 +0700

    feat: manual auto slide without plugin (#9)

--- components/SlideShip.vue
+++ components/SlideShip.vue
@@ -1,5 +1,5 @@
 <template>
-  <Flicking ref="flicking" @will-change="willChange" :plugins="plugins" :options="{ align: 'prev', circular: false }">
+  <Flicking ref="flicking" @ready="ready" @will-change="willChange" :options="{ align: 'prev', circular: false }">
     <div class="panel" v-for="ship in ships" :key="ship.id">
       <img :src="ship.image" width="300" class="pointer-events-none" />
     </div>
@@ -11,8 +11,6 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { AutoPlay } from '@egjs/flicking-plugins'
-
 
 export default Vue.extend({
   props: {
@@ -24,12 +22,29 @@ export default Vue.extend({
   data() {
     return {
       activeIndex: 0,
-      plugins: [
-        new AutoPlay({ duration: 2000, direction: "NEXT", stopOnHover: false })
-      ]
+      duration: 3_000
     }
   },
   methods: {
+    ready() {
+      const flicking = this.$refs.flicking as any
+
+      if (!flicking) return
+
+      if (flicking.animating) return
+
+      window.setTimeout(() => {
+        const status = flicking.getStatus()
+
+        if (status.index === status.panels.length - 1) {
+          flicking.moveTo(0).catch(() => void 0)
+        } else {
+          flicking.next().catch(() => void 0)
+        }
+
+        this.ready()
+      }, this.duration)
+    },
     move(index: number) {
       const flicking = this.$refs.flicking as any
       if (!flicking.animating) {
@@ -42,3 +57,27 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style scoped>
+.flicking-pagination {
+  position: absolute;
+  left: 0;
+  bottom: 10px;
+  width: 100%;
+  text-align: center;
+  z-index: 2;
+}
+.flicking-pagination-bullet {
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  margin: 0 4px;
+  border-radius: 50%;
+  cursor: pointer;
+  font-size: 1rem;
+  background-color: rgb(10 10 10/10%);
+}
+.flicking-pagination-bullet.flicking-pagination-bullet-active {
+  background-color: #f2a65e;
+}
+</style>
