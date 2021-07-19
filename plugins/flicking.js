dd2db1ae410d116c3795db82300c5feefbb58059
hawkup <spiritsquare@gmail.com>
2021-07-19 21:55:22 +0700

    feat: manual auto slide without plugin

--- plugins/flicking.js
+++ plugins/flicking.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Flicking from '@egjs/vue-flicking'
 import '@egjs/vue-flicking/dist/flicking.css'
-import '@egjs/flicking-plugins/dist/flicking-plugins.css'
 
 Vue.use(Flicking)
