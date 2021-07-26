0901d0641e29d47e4b21d1c6903ace63634b82ce
hawkup <spiritsquare@gmail.com>
2021-07-26 10:17:50 +0700

    feat: add dinero (#25)

---
+++ pages/money.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <pre>{{ price }}</pre>
+    <pre>{{ shipping }}</pre>
+    <pre>{{ total }}</pre>
+    <pre>{{ bahtTotal }}</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { dinero, add, convert, toFormat, Dinero } from 'dinero.js'
+import { USD, THB } from '@dinero.js/currencies'
+
+// https://martinfowler.com/eaaCatalog/money.html
+// https://v2.dinerojs.com/docs
+export default Vue.extend({
+  data() {
+    return {
+      price: dinero({ amount: 0, currency: USD }) as Dinero<number>,
+      shipping: dinero({ amount: 0, currency: USD }) as Dinero<number>,
+      total: dinero({ amount: 0, currency: USD }) as Dinero<number>,
+      rates: { THB: 31 },
+      bahtTotal: ''
+    }
+  },
+  methods: {
+    format: (dineroObject: Dinero<number>) => {
+      return toFormat(
+        dineroObject,
+        ({ amount, currency }) => `${currency.code} ${amount}`
+      );
+    }
+  },
+  mounted() {
+    this.price = dinero({ amount: 5000, currency: USD })
+    this.shipping = dinero({ amount: 100, currency: USD })
+    this.total = add(this.price, this.shipping)
+    this.bahtTotal = this.format(convert(this.total, THB, this.rates))
+  }
+})
+</script>
