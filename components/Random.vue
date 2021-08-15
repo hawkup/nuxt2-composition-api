d1305d8aea2d971752e835ef2ec290e0a1c8f4b4
hawkup <spiritsquare@gmail.com>
2021-08-15 12:17:36 +0700

    feat: add keep-alive example

---
+++ components/Random.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>random: {{ random }}</p>
+    <p>info: {{ info }}</p>
+    <input type="text" class="border" v-model="info" />
+  </div>
+</template>
+
+<script lang="ts">
+import vue from 'vue'
+import { ref } from '@nuxtjs/composition-api'
+
+export default vue.extend({
+  setup() {
+    const random = Math.random()
+    const info = ref()
+    return { random, info }
+  }
+})
+</script>
---
+++ pages/keepAlive/two.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>two</h1>
+    <div>
+      <p>random: {{ random }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import vue from 'vue'
+
+export default vue.extend({
+  setup() {
+    const random = Math.random()
+    return { random }
+  }
+})
+</script>
