fe072766c13912812b4f18b9175602a25fab74bd
hawkup <spiritsquare@gmail.com>
2021-08-25 02:09:29 +0700

    feat: add handle notfound/null (#40)

---
+++ pages/notfound/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ id }}</div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, useRoute } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const route = useRoute()
+    const id = computed(() => route.value.params.id)
+    return {
+      id
+    }
+  }
+})
+</script>
