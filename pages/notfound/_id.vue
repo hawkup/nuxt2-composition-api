a1bdc1bb33076a6d7225bb6aa191546a5238b337
hawkup <spiritsquare@gmail.com>
2021-08-25 02:05:15 +0700

    feat: add handle notfound/null

---
+++ pages/notfound/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <nuxt-child keep-alive />
+</template>
+
+<script lang="ts">
+export default {
+  mounted() {
+    if (!this.$route.params.id) {
+      this.$nuxt.error({ statusCode: 404, message: 'Not found' })
+    }
+  }
+}
+</script>
