b53c7e35e844d99b42c82a71c5fe97bc887a025f
hawkup <spiritsquare@gmail.com>
2021-08-30 02:34:19 +0700

    feat: chunk page testing

---
+++ pages/chunk/_id.vue
@@ -0,0 +1,7 @@
+<template>
+  <nuxt-child keep-alive />
+</template>
+
+<script>
+console.log('chunk layout')
+</script>
---
+++ pages/chunk/_id/video/_videoId.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>chunk/_id/video/_videoId</div>
+</template>
+
+<script>
+console.log('chunk/_id/video/_videoId')
+</script>
