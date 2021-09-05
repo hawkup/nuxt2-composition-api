3e584dc7b9d97ea9887633de260593381aa3fa05
hawkup <spiritsquare@gmail.com>
2021-09-05 15:37:53 +0700

    feat: make /chunk import many components

--- pages/chunk/_id/book/_bookId.vue
+++ pages/chunk/_id/book/_bookId.vue
@@ -2,6 +2,32 @@
   <div>chunk/_id/book/_bookId</div>
 </template>
 
-<script>
-console.log('chunk/_id/book/_bookId')
+<script lang="ts">
+import Vue from 'vue'
+
+import Abort from '~/components/Abort.vue'
+import Capsule from '~/components/Capsule.vue'
+import Carousel from '~/components/Carousel.vue'
+import Large from '~/components/Large.vue'
+import Mission from '~/components/Mission.vue'
+import Ship from '~/components/Ship.vue'
+import ShipList from '~/components/ShipList.vue'
+import Swipe from '~/components/Swipe.vue'
+import TodoList from '~/components/TodoList.vue'
+import Tutorial from '~/components/Tutorial.vue'
+
+export default Vue.extend({
+  components: {
+    Abort,
+    Capsule,
+    Carousel,
+    Large,
+    Mission,
+    Ship,
+    ShipList,
+    Swipe,
+    TodoList,
+    Tutorial
+  }
+})
 </script>
