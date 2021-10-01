b00615b92d42beab32a7cf34e7f13aaaefa4f35b
hawkup <spiritsquare@gmail.com>
2021-10-01 23:33:17 +0700

    feat: add vue-lazy-hydration (#55)

--- pages/hydrate.vue
+++ pages/hydrate.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    <h1>Hydrate</h1>
-  </div>
+  <LazyHydrate never>
+    <div>
+      <h1>Hydrate</h1>
+    </div>
+  </LazyHydrate>
 </template>
 
 <script>
+import LazyHydrate from 'vue-lazy-hydration'
+
 export default {
+  components: {
+    LazyHydrate
+  }
 }
 </script>
