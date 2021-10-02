019d224cbf5436bb72011f48e9502df4a583dc3f
hawkup <spiritsquare@gmail.com>
2021-10-02 12:48:36 +0700

    feat: add more element

--- pages/hydrate.vue
+++ pages/hydrate.vue
@@ -3,12 +3,15 @@
     <LazyHydrate never>
       <h1>Hydrate</h1>
     </LazyHydrate>
-    <LazyHydrate when-idle>
+    <LazyHydrate never>
       <div>
+        <Mission v-for="mission in missions" :key="mission.id" :data="mission" />
+        <Mission v-for="mission in missions" :key="mission.id" :data="mission" />
+        <Mission v-for="mission in missions" :key="mission.id" :data="mission" />
         <Mission v-for="mission in missions" :key="mission.id" :data="mission" />
       </div>
     </LazyHydrate>
-    <LazyHydrate when-idle>
+    <LazyHydrate when-visible>
       <ShipList />
     </LazyHydrate>
   </div>
