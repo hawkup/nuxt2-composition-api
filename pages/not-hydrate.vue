e4916bdddc3f04953255b3dbf9e8aa32f9bbeaa6
hawkup <spiritsquare@gmail.com>
2021-10-02 12:18:19 +0700

    feat: add not-hydrate page and add some component

---
+++ pages/not-hydrate.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Not Hydrate</h1>
+    <Mission v-for="mission in missions" :key="mission.id" :data="mission" />
+    <ShipList />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const missions = await app.$axios.$get('https://api.spacex.land/rest/missions')
+    return { missions }
+  }
+}
+</script>
