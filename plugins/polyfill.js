e4dc4e424c0c030fe40e114f6bb69c15b6266d3c
hawkup <spiritsquare@gmail.com>
2021-10-03 22:47:31 +0700

    fix: comment polyfill

--- plugins/polyfill.js
+++ plugins/polyfill.js
@@ -1,17 +1,17 @@
 export default function() {
-  if (!Array.prototype.flat) {
-    console.log('browser not support flat')
-    setTimeout(() => {
-      console.log('import flat polyfill')
-      try {
-        import('array-flat-polyfill')
-      } catch (e) {
-        console.log(e)
-      }
-    }, 5000)
-  } else {
-    console.log('browser support flat')
-  }
+  // if (!Array.prototype.flat) {
+  //   console.log('browser not support flat')
+  //   setTimeout(() => {
+  //     console.log('import flat polyfill')
+  //     try {
+  //       import('array-flat-polyfill')
+  //     } catch (e) {
+  //       console.log(e)
+  //     }
+  //   }, 5000)
+  // } else {
+  //   console.log('browser support flat')
+  // }
 
   if (!Array.prototype.flat) {
     console.log('first polyfill broken')
