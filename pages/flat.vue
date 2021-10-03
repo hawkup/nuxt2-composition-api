28c5240658950669a6815e8924888a9087f476f5
hawkup <spiritsquare@gmail.com>
2021-10-03 20:53:04 +0700

    feat: add flatMap (#65)

--- pages/flat.vue
+++ pages/flat.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <div>Array.prototype.flat: {{ Array.prototype.flat }}</div>
+    <div>Array.prototype.flatMap: {{ Array.prototype.flatMap }}</div>
     <div>array: {{ [0, 1, 2, [3, 4]] }}</div>
     <div>flat: {{ [0, 1, 2, [3, 4]].flat() }}</div>
+    <div>flatMap: {{ [1, 2, 3, 4].flatMap(x => [x, x * 2]) }}</div>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    console.log('[].flat()', [].flat())
+    console.log('[1, 2, 3, 4].flatMap(x => [x, x * 2])', [1, 2, 3, 4].flatMap(x => [x, x * 2]))
+  }
+}
+</script>
