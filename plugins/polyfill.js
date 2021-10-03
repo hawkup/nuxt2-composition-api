675f2918f53def8f22e0cfb96370ac6cbec32c8d
hawkup <spiritsquare@gmail.com>
2021-10-03 20:28:04 +0700

    feat: add old method to add script

--- plugins/polyfill.js
+++ plugins/polyfill.js
@@ -1,9 +1,23 @@
 export default function() {
   if (!Array.prototype.flat) {
-    console.log('not support flat')
+    console.log('browser not support flat')
     setTimeout(() => {
       console.log('import flat polyfill')
-      import('array-flat-polyfill')
+      try {
+        import('array-flat-polyfill')
+      } catch (e) {
+        console.log(e)
+      }
     }, 5000)
+  } else {
+    console.log('browser support flat')
+  }
+
+  if (!Array.prototype.flat) {
+    console.log('first polyfill broken')
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src = 'https://polyfill.io/v3/polyfill.min.js?features=Array.prototype.flat'
+    document.head.appendChild(script)
   }
 }
