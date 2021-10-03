30b76ad14a80f4c1fa537f67d7b35a63c2e4dca4
hawkup <spiritsquare@gmail.com>
2021-10-03 19:36:51 +0700

    delay polyfill (#62)

--- plugins/polyfill.js
+++ plugins/polyfill.js
@@ -1,5 +1,9 @@
 export default function() {
-  if (!Array.prototype.flat) {
-    import('array-flat-polyfill')
+  if (Array.prototype.flat) {
+    console.log('not support flat')
+    setTimeout(() => {
+      console.log('import flat polyfill')
+      import('array-flat-polyfill')
+    }, 5000)
   }
 }
