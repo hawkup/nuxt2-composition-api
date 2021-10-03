fa2515c6d93ae3ebeb839347c2ecee8784e21ea8
hawkup <spiritsquare@gmail.com>
2021-10-03 19:08:02 +0700

    feat: add example .flat() (#61)

---
+++ pages/flat.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div>Array.prototype.flat: {{ Array.prototype.flat }}</div>
+    <div>array: {{ [0, 1, 2, [3, 4]] }}</div>
+    <div>flat: {{ [0, 1, 2, [3, 4]].flat() }}</div>
+  </div>
+</template>
