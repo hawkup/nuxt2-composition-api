2fbfdeca5766c8c92ecd9c3966d9f6dfb5d5e59e
hawkup <spiritsquare@gmail.com>
2021-10-03 23:07:39 +0700

    feat: rollback polyfill

--- plugins/polyfill.js
+++ plugins/polyfill.js
@@ -1,17 +1,17 @@
 export default function() {
-  // if (!Array.prototype.flat) {
-  //   console.log('browser not support flat')
-  //   setTimeout(() => {
-  //     console.log('import flat polyfill')
-  //     try {
-  //       import('array-flat-polyfill')
-  //     } catch (e) {
-  //       console.log(e)
-  //     }
-  //   }, 5000)
-  // } else {
-  //   console.log('browser support flat')
-  // }
+  if (!Array.prototype.flat) {
+    console.log('browser not support flat')
+    setTimeout(() => {
+      console.log('import flat polyfill')
+      try {
+        import('array-flat-polyfill')
+      } catch (e) {
+        console.log(e)
+      }
+    }, 5000)
+  } else {
+    console.log('browser support flat')
+  }
 
   if (!Array.prototype.flat) {
     console.log('first polyfill broken')
@@ -19,15 +19,5 @@ export default function() {
     script.type = 'text/javascript'
     script.src = 'https://unpkg.com/array-flat-polyfill'
     document.head.appendChild(script)
-  }
-
-  if (!Promise.allSettled) {
-    alert('not support Promise.allSettled')
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.src = 'https://unpkg.com/@ungap/promise-all-settled'
-    document.head.appendChild(script)
-  } else {
-    alert('support Promise.allSettled')
   }
 }
